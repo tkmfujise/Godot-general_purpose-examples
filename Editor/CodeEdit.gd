885a937a4471e8ec7cfa8cac8846fbc62ff1d60e
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2023-07-16 01:57:37 +0900

    ソース整理。テーマを追加。また、シンタックスに HTML 追加

--- Editor/CodeEdit.gd
+++ Editor/CodeEdit.gd
@@ -1,64 +1,40 @@
 extends CodeEdit
 
-const COLORS = {
-    "background": "#062037",
-    "foreground": "#FFFFFF",
-    "punctuation": "#FFFFFF",
-    "symbol": "#FBFBD3",
-    "constant": "#DCF710",
-    "boolean": "#98E1FF",
-    "entity": "#FDFF9A",
-    "type_name": "#FFB3E9",
-    "function_color": "#F9B7DC",
-    "keyword": "#F4B043",
-    "number": "#FFFFFF",
-    "string": "#46F009",
-    "comment": "#5DA9EC",
-}
-
-
-var current_lang : String
 var current_syntax : SyntaxResource
+var current_theme : ThemeResource
 
 
 func _ready() -> void:
     syntax_highlighter = CodeHighlighter.new()
-    set_code_theme("Gessetti")
-    set_code_language("ruby")
 
 
-func set_code_theme(theme: String) -> void:
-    add_theme_color_override("background_color", COLORS["background"])
-    add_theme_color_override("font_color", COLORS["foreground"])
-    syntax_highlighter.set_symbol_color(COLORS["symbol"])
-    syntax_highlighter.set_number_color(COLORS["number"])
-    syntax_highlighter.set_function_color(COLORS["function_color"])
+func set_code_theme(theme: ThemeResource) -> void:
+    current_theme = theme
+    add_theme_color_override("background_color", theme.background_color)
+    add_theme_color_override("font_color", theme.foreground_color)
+    syntax_highlighter.set_symbol_color(theme.symbol_color)
+    syntax_highlighter.set_number_color(theme.number_color)
+    syntax_highlighter.set_function_color(theme.function_color)
 
 
-func set_code_language(lang: String) -> void:
-    current_lang = lang
-    var resource = load("res://syntax/%s-syntax.tres" % lang)
-    clear_syntax()
-    set_syntax(resource)
-
-
-func set_syntax(resource: SyntaxResource) -> void:
-    current_syntax = resource
-    indent_size    = resource.indent_size
-    for key in resource.regions:
-        for arr in resource.regions[key]:
-            if COLORS.has(key):
+func set_syntax(syntax: SyntaxResource) -> void:
+    clear_syntax_highlighter()
+    current_syntax = syntax
+    indent_size = syntax.indent_size
+    for key in syntax.regions:
+        for arr in syntax.regions[key]:
+            if current_theme.syntax_colors.has(key):
                 syntax_highlighter.add_color_region(
-                    arr[0], arr[1], COLORS[key]
+                    arr[0], arr[1], current_theme.syntax_colors[key]
                 )
 
-    for key in resource.keywords:
-        for value in resource.keywords[key]:
-            if COLORS.has(key):
-                syntax_highlighter.add_keyword_color(value, COLORS[key])
+    for key in syntax.keywords:
+        for value in syntax.keywords[key]:
+            if current_theme.syntax_colors.has(key):
+                syntax_highlighter.add_keyword_color(value, current_theme.syntax_colors[key])
 
 
-func clear_syntax():
+func clear_syntax_highlighter():
     syntax_highlighter.clear_highlighting_cache()
     syntax_highlighter.clear_color_regions()
     syntax_highlighter.clear_keyword_colors()
--- Editor/Main.gd
+++ Editor/Main.gd
@@ -1,5 +1,22 @@
 extends Control
 
+var current_lang : String
+var current_syntax : String
+var current_theme : String
 
 func _ready() -> void:
     $CodeEdit.grab_focus()
+    set_code_theme("Gessetti")
+    set_syntax("Ruby")
+
+
+func set_code_theme(theme: String) -> void:
+    current_theme = theme
+    var resource  = load("res://themes/%s-theme.tres" % theme)
+    $CodeEdit.set_code_theme(resource)
+
+
+func set_syntax(lang: String) -> void:
+    current_lang = lang
+    var resource = load("res://syntaxes/%s-syntax.tres" % lang)
+    $CodeEdit.set_syntax(resource)
