35681d174e8269d28c978b0aa1c73d75618b0ee4
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2025-04-27 17:25:52 +0900

    update Godot v4.4

--- Editor/FileMenu.gd
+++ Editor/FileMenu.gd
@@ -1,28 +1,28 @@
 extends MenuButton
 
 const SHORTCUT_KEY_MAPPING = {
-    0: "file_new",
-    1: "file_open",
-    2: "file_save",
-    3: "file_save_as",
+	0: "file_new",
+	1: "file_open",
+	2: "file_save",
+	3: "file_save_as",
 }
 
 
 func _ready() -> void:
-    add_shortcut_key_lables_to_popup(get_popup())
+	add_shortcut_key_lables_to_popup(get_popup())
 
 
 func add_shortcut_key_lables_to_popup(popup: PopupMenu) -> void:
-    for i in item_count:
-        var id = popup.get_item_id(i)
-        if SHORTCUT_KEY_MAPPING.has(id):
-            var _str = popup.get_item_text(i)
-            popup.set_item_text(i, _str + \
-                "  (" + shortcut_key_of(SHORTCUT_KEY_MAPPING[id]) + ")"
-            )
+	for i in item_count:
+		var id = popup.get_item_id(i)
+		if SHORTCUT_KEY_MAPPING.has(id):
+			var _str = popup.get_item_text(i)
+			popup.set_item_text(i, _str + \
+				"  (" + shortcut_key_of(SHORTCUT_KEY_MAPPING[id]) + ")"
+			)
 
 
 func shortcut_key_of(event_name: String) -> String:
-    var event = InputMap.action_get_events(event_name)[0]
-    var _str = OS.get_keycode_string(event.get_keycode_with_modifiers())
-    return _str.replace("Command", "⌘")
+	var event = InputMap.action_get_events(event_name)[0]
+	var _str = OS.get_keycode_string(event.get_keycode_with_modifiers())
+	return _str.replace("Command", "⌘")
--- Editor/Main.gd
+++ Editor/Main.gd
@@ -5,9 +5,9 @@ enum ViewMenuId { THEME }
 
 const NEW_FILE_PLACEHOLDER = "Untitled"
 const EXTENSIONS = {
-    "Ruby": ["rb", "erb", "ruby"],
-    "HTML": ["html", "htm"],
-    "Markdown": ["md", "markdown"],
+	"Ruby": ["rb", "erb", "ruby"],
+	"HTML": ["html", "htm"],
+	"Markdown": ["md", "markdown"],
 }
 const LANG_PLACEHOLDER = "Text"
 const THEMES = ["Gessetti", "Pennarelli"]
@@ -20,182 +20,180 @@ var file_dirty : bool
 
 
 func _ready() -> void:
-    $TopBar/FileMenu.get_popup() \
-        .id_pressed.connect(_on_file_menu_selected)
-    $BottomBar/IndentSizeMenu.get_popup() \
-        .id_pressed.connect(_on_indent_changed_from_menu)
-    $BottomBar/SyntaxMenu.get_popup() \
-        .id_pressed.connect(_on_syntax_changed_from_menu)
-    generate_theme_menu_items()
-    generate_syntax_menu_items()
-    $CodeArea.grab_focus()
-    new_file()
+	$TopBar/FileMenu.get_popup() \
+		.id_pressed.connect(_on_file_menu_selected)
+	$BottomBar/IndentSizeMenu.get_popup() \
+		.id_pressed.connect(_on_indent_changed_from_menu)
+	$BottomBar/SyntaxMenu.get_popup() \
+		.id_pressed.connect(_on_syntax_changed_from_menu)
+	generate_theme_menu_items()
+	generate_syntax_menu_items()
+	$CodeArea.grab_focus()
+	new_file()
 
 
 func update_window_title() -> void:
-    var title = \
-        ProjectSettings.get_setting("application/config/name") \
-        + ' - ' + current_file_name()
-    if file_dirty: title += " *"
-    get_window().title = title
+	var title = \
+		ProjectSettings.get_setting("application/config/name") \
+		+ ' - ' + current_file_name()
+	if file_dirty: title += " *"
+	get_window().title = title
 
 
 func new_file():
-    current_file   = ''
-    $CodeArea.text = ''
-    $CodeArea.clear_syntax()
-    file_dirty = false
-    update_window_title()
+	current_file   = ''
+	$CodeArea.text = ''
+	$CodeArea.clear_syntax()
+	file_dirty = false
+	update_window_title()
 
 
 func current_file_name() -> String:
-    if current_file:
-        return current_file.get_file()
-    else:
-        return NEW_FILE_PLACEHOLDER
+	if current_file:
+		return current_file.get_file()
+	else:
+		return NEW_FILE_PLACEHOLDER
 
 
 func save_file():
-    if current_file:
-        var path = current_file
-        var f = FileAccess.open(path, FileAccess.WRITE)
-        f.store_string($CodeArea.text)
-        f.close()
-        current_file = path
-        file_dirty = false
-        update_window_title()
-    else:
-        $SaveFileDialog.popup()
+	if current_file:
+		var path = current_file
+		var f = FileAccess.open(path, FileAccess.WRITE)
+		f.store_string($CodeArea.text)
+		f.close()
+		current_file = path
+		file_dirty = false
+		update_window_title()
+	else:
+		$SaveFileDialog.popup()
 
 
 func set_code_theme(_theme: String) -> void:
-    current_theme = _theme
-    var resource  = load("res://code_themes/%s-theme.tres" % _theme)
-    $CodeArea.set_code_theme(resource)
+	current_theme = _theme
+	var resource  = load("res://code_themes/%s-theme.tres" % _theme)
+	$CodeArea.set_code_theme(resource)
 
 
 func generate_theme_menu_items():
-    var popup = $TopBar/ViewMenu.get_popup()
-    var submenu = PopupMenu.new()
-    submenu.set_name("ThemeMenu")
-    for _name in THEMES:
-        submenu.add_radio_check_item(_name)
-    popup.add_child(submenu)
-    popup.add_submenu_item("Theme", "ThemeMenu", ViewMenuId.THEME)
-    submenu.set_item_checked(0, true)
-    submenu.id_pressed.connect(_on_code_theme_changed)
-    set_code_theme(THEMES[0])
+	var popup = $TopBar/ViewMenu.get_popup()
+	var submenu = PopupMenu.new()
+	submenu.set_name("ThemeMenu")
+	for _name in THEMES:
+		submenu.add_radio_check_item(_name)
+	popup.add_child(submenu)
+	popup.add_submenu_item("Theme", "ThemeMenu", ViewMenuId.THEME)
+	submenu.set_item_checked(0, true)
+	submenu.id_pressed.connect(_on_code_theme_changed)
+	set_code_theme(THEMES[0])
 
 
 func set_lang(lang: String) -> void:
-    current_lang = lang
-    $BottomBar/SyntaxMenu.text = lang
-    if lang == LANG_PLACEHOLDER:
-        $CodeArea.clear_syntax()
-    else:
-        set_syntax(lang)
+	current_lang = lang
+	$BottomBar/SyntaxMenu.text = lang
+	if lang == LANG_PLACEHOLDER:
+		$CodeArea.clear_syntax()
+	else:
+		set_syntax(lang)
 
 
 func guess_syntax() -> void:
-    var ext = current_file.get_extension()
-    for key in EXTENSIONS:
-        if EXTENSIONS[key].has(ext):
-            set_lang(key)
-            return
-    set_lang(LANG_PLACEHOLDER)
+	var ext = current_file.get_extension()
+	for key in EXTENSIONS:
+		if EXTENSIONS[key].has(ext):
+			set_lang(key)
+			return
+	set_lang(LANG_PLACEHOLDER)
 
 
 func set_syntax(lang: String) -> void:
-    var resource = load("res://code_syntaxes/%s-syntax.tres" % lang)
-    $CodeArea.set_syntax(resource)
+	var resource = load("res://code_syntaxes/%s-syntax.tres" % lang)
+	$CodeArea.set_syntax(resource)
 
 
 func generate_syntax_menu_items() -> void:
-    var popup = $BottomBar/SyntaxMenu.get_popup()
-    popup.set_item_count(EXTENSIONS.size() + 1)
-    var i = 0
-    for key in EXTENSIONS:
-        popup.set_item_text(i, key)
-        i += 1
-    popup.set_item_text(i, LANG_PLACEHOLDER)
-    set_lang(LANG_PLACEHOLDER)
+	var popup = $BottomBar/SyntaxMenu.get_popup()
+	popup.set_item_count(EXTENSIONS.size() + 1)
+	var i = 0
+	for key in EXTENSIONS:
+		popup.set_item_text(i, key)
+		i += 1
+	popup.set_item_text(i, LANG_PLACEHOLDER)
+	set_lang(LANG_PLACEHOLDER)
 
 
 func _input(event):
-    if event.is_action_pressed("file_new"):
-        new_file()
-    elif event.is_action_pressed("file_open"):
-        $OpenFileDialog.popup()
-    elif event.is_action_pressed("file_save_as"):
-        $SaveFileDialog.popup()
-    elif event.is_action_pressed("file_save"):
-        save_file()
+	if event.is_action_pressed("file_new"):
+		new_file()
+	elif event.is_action_pressed("file_open"):
+		$OpenFileDialog.popup()
+	elif event.is_action_pressed("file_save_as"):
+		$SaveFileDialog.popup()
+	elif event.is_action_pressed("file_save"):
+		save_file()
 
 
 func _on_file_menu_selected(id: int) -> void:
-    match id:
-        FileMenuId.NEW:
-            new_file()
-        FileMenuId.OPEN:
-            $OpenFileDialog.popup()
-        FileMenuId.SAVE:
-            save_file()
-        FileMenuId.SAVE_AS:
-            $SaveFileDialog.popup()
-        FileMenuId.QUIT:
-            get_tree().quit()
+	match id:
+		FileMenuId.NEW:
+			new_file()
+		FileMenuId.OPEN:
+			$OpenFileDialog.popup()
+		FileMenuId.SAVE:
+			save_file()
+		FileMenuId.SAVE_AS:
+			$SaveFileDialog.popup()
+		FileMenuId.QUIT:
+			get_tree().quit()
 
 
 func _on_indent_changed_from_menu(id_as_indent: int) -> void:
-    $CodeArea.set_indent(id_as_indent)
+	$CodeArea.set_indent(id_as_indent)
 
 
 func _on_code_area_indent_changed(indent: int) -> void:
-    var text = $BottomBar/IndentSizeMenu.text
-    var regex = RegEx.new()
-    regex.compile("\\d+")
-    $BottomBar/IndentSizeMenu.text = regex.sub(text, str(indent))
+	var text = $BottomBar/IndentSizeMenu.text
+	var regex = RegEx.new()
+	regex.compile("\\d+")
+	$BottomBar/IndentSizeMenu.text = regex.sub(text, str(indent))
 
 
 func _on_code_theme_changed(index: int) -> void:
-    set_code_theme(THEMES[index])
-    var popup = $TopBar/ViewMenu.get_popup()
-    var submenu_name = popup.get_item_submenu(ViewMenuId.THEME)
-    var submenu = popup.get_node(submenu_name)
-    for i in submenu.item_count:
-        submenu.set_item_checked(i, false)
-    submenu.set_item_checked(index, true)
+	set_code_theme(THEMES[index])
+	var popup = $TopBar/ViewMenu.get_popup()
+	var submenu_name = popup.get_item_submenu(ViewMenuId.THEME)
+	var submenu = popup.get_node(submenu_name)
+	for i in submenu.item_count:
+		submenu.set_item_checked(i, false)
+	submenu.set_item_checked(index, true)
 
 
 func _on_syntax_changed_from_menu(id_as_index: int) -> void:
-    var popup = $BottomBar/SyntaxMenu.get_popup()
-    var lang  = popup.get_item_text(id_as_index)
-    set_lang(lang)
-    if lang != LANG_PLACEHOLDER: set_syntax(lang)
+	var popup = $BottomBar/SyntaxMenu.get_popup()
+	var lang  = popup.get_item_text(id_as_index)
+	set_lang(lang)
+	if lang != LANG_PLACEHOLDER: set_syntax(lang)
 
 
 func _on_open_file_dialog_file_selected(path: String) -> void:
-    var f = FileAccess.open(path, FileAccess.READ)
-    $CodeArea.text = f.get_as_text()
-    f.close()
-    current_file = path
-    guess_syntax()
-    file_dirty = false
-    update_window_title()
+	var f = FileAccess.open(path, FileAccess.READ)
+	$CodeArea.text = f.get_as_text()
+	f.close()
+	current_file = path
+	guess_syntax()
+	file_dirty = false
+	update_window_title()
 
 
 func _on_save_file_dialog_file_selected(path: String) -> void:
-    var f = FileAccess.open(path, FileAccess.WRITE)
-    f.store_string($CodeArea.text)
-    f.close()
-    current_file = path
-    file_dirty = false
-    update_window_title()
+	var f = FileAccess.open(path, FileAccess.WRITE)
+	f.store_string($CodeArea.text)
+	f.close()
+	current_file = path
+	file_dirty = false
+	update_window_title()
 
 
 func _on_code_area_text_changed() -> void:
-    if file_dirty: return
-    file_dirty = true
-    update_window_title()
-
-
+	if file_dirty: return
+	file_dirty = true
+	update_window_title()
